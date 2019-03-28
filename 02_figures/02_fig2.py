66ac82b86425d47a8595083b21d73375e73c5652
Angus Fisk <angus.fisk@ndcn.ox.ac.uk>
2019-03-28 12:22:13 +0000

    Stats figure 2
    All first draft of stats for figure 2, still need to fill in on the
    plots but damn good start

--- 02_figures/02_fig2.py
+++ 02_figures/02_fig2.py
@@ -6,6 +6,7 @@ import pathlib
 import pandas as pd
 idx = pd.IndexSlice
 import numpy as np
+import pingouin as pg
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gs
@@ -43,7 +44,7 @@ df_list[-1] = sj_df
 df_dict = dict(zip(file_names, df_list))
 all_df = pd.concat(df_dict, sort=False)
 
-############################
+################################################################################
 # compute the mean waveforms
 
 # split each animal and create dataframe out of it
@@ -109,7 +110,7 @@ lights = light_df.where(light_mask, other=500)
 lights = lights.mask(light_mask, other=0)
 lights = lights.mean(axis=1)
 
-############ Calculate Qp IS IV
+############ Calculate Qp IS IV ################################################
 
 col_names = ["condition", "animal", "value"]
 def longform_df(data, col_names):
@@ -130,22 +131,130 @@ power_vals.drop("LDR", axis=1, inplace=True)
 # get number of days and divide power_vals by that
 timespan = (df_list[0].index[-1] - df_list[0].index[0]).round("D").days
 interdaily_stab = power_vals / timespan
-power_vals = longform_df(power_vals, col_names)
-interdaily_stab = longform_df(interdaily_stab, col_names)
+long_power_vals = longform_df(power_vals, col_names)
+long_interdaily_stab = longform_df(interdaily_stab, col_names)
 
 # Calculate IV
 intraday_var = als.intradayvar(all_df, level=[0])
 intraday_var.drop("LDR", axis=1, inplace=True)
-intraday_var = longform_df(intraday_var, col_names)
+long_intraday_var = longform_df(intraday_var, col_names)
 
 # put together in a list
 marker_dict = {
-    "Qp": power_vals,
-    "IS": interdaily_stab,
-    "IV": intraday_var
+    "Qp": long_power_vals,
+    "IS": long_interdaily_stab,
+    "IV": long_intraday_var
 }
 
-###########
+# Values for total activity per day ############################################
+
+# get the values
+daily = all_df.groupby(level=0).resample("D", level=1).mean()
+daily.drop("LDR", axis=1, inplace=True)
+daily_mean = daily.mean(axis=1).unstack(level=0)
+daily_sem = daily.sem(axis=1).unstack(level=0)
+
+
+################################################################################
+# stats
+
+stats_colnames = ["Protocol", "Hour", "Animal", "Value"]
+protocol_col = stats_colnames[0]
+anim_col = stats_colnames[2]
+hour_col = stats_colnames[1]
+dep_var = stats_colnames[3]
+hours = split_df_h.index.get_level_values(-1).unique()
+
+save_test_dir = pathlib.Path("/Users/angusfisk/Documents/01_PhD_files/"
+                             "01_projects/01_thesisdata/04_ageing/"
+                             "03_analysisoutputs/01_figures/00_csvs/02_fig2")
+anova_str = "01_anova.csv"
+ph_str = "02_posthoc.csv"
+
+# Q1. Is the mean activity profile different between groups?
+# 2 way anova activity ~ protocol*hour with ph activity ~ protocol | hour
+
+mean_test_dir = save_test_dir / "01_mean"
+split_labels = ["Total", "First", "Last"]
+for split_df, label in zip([split_df_h, first_week, last_week], split_labels):
+    print(label)
+    label_test_dir = mean_test_dir / label
+    # tidy data
+    test_df = split_df.mean(axis=1)
+    test_df.drop("LDR", level=1, inplace=True)
+    label_df = test_df.unstack(level=1
+                               ).groupby(level=0
+                                         ).apply(prep.label_anim_cols)
+    tidy_df = label_df.stack().reset_index()
+    tidy_df.columns = stats_colnames
+    print(tidy_df.head())
+
+    # perform anova
+    anova = pg.mixed_anova(dv=dep_var,
+                           between=protocol_col,
+                           within=hour_col,
+                           subject=anim_col,
+                           data=tidy_df)
+    pg.print_table(anova)
+
+    # do post hocs
+    ph_df = prep.tukey_pairwise_ph(tidy_df)
+    
+    anova.to_csv((label_test_dir / anova_str))
+    ph_df.to_csv((label_test_dir / ph_str))
+    
+
+# Q2. Does the condition affect Qp/IV/IS?
+# 1 way anova of values for each
+
+marker_test_dir = save_test_dir / "02_markers"
+for label, df in zip(marker_dict.keys(),
+                     [power_vals, interdaily_stab, intraday_var]):
+    print(label)
+    marker_label_test_dir = marker_test_dir / label
+    marker_df = df
+    label_df = marker_df.groupby(level=0
+                                 ).apply(prep.label_anim_cols
+                                         ).stack(
+                                                 ).reset_index()
+    label_df.columns = [stats_colnames[x] for x in [0, 2, 3]]
+    print(label_df.head())
+    
+    # anova
+    marker_anova = pg.anova(dv=dep_var,
+                            between=protocol_col,
+                            data=label_df)
+    pg.print_table(marker_anova)
+    
+    marker_anova.to_csv((marker_label_test_dir / anova_str))
+
+# Q3. Does the total activity change between groups?
+# 2 way anova of activity ~ day*protocol with activity ~ protocol | day
+
+tot_act_test_dir = save_test_dir / "03_tot_act"
+act_df = daily
+label_df = act_df.groupby(level=0
+                          ).apply(prep.label_anim_cols
+                                  ).stack(
+                                          ).reset_index()
+label_df.columns = stats_colnames
+print(label_df.head())
+
+# anova
+activity_anova = pg.mixed_anova(dv=dep_var,
+                                between=protocol_col,
+                                within=hour_col,
+                                subject=anim_col,
+                                data=label_df)
+pg.print_table(activity_anova)
+
+ph = prep.tukey_pairwise_ph(label_df)
+
+activity_anova.to_csv((tot_act_test_dir / anova_str))
+ph.to_csv((tot_act_test_dir / ph_str))
+
+
+################################################################################
 # plot
 
 # plot constants
@@ -218,11 +327,6 @@ activity_grid = gs.GridSpec(nrows=1, ncols=1, figure=fig, left=0.55, top=0.45)
 activity_axes = [plt.subplot(x) for x in activity_grid]
 ac_axis = activity_axes[0]
 
-# get the values
-daily = all_df.groupby(level=0).resample("D", level=1).mean()
-daily.drop("LDR", axis=1, inplace=True)
-daily_mean = daily.mean(axis=1).unstack(level=0)
-daily_sem = daily.sem(axis=1).unstack(level=0)
 # plot on the axis
 for condition in daily_mean.columns:
     mean = daily_mean.loc[:, condition]
